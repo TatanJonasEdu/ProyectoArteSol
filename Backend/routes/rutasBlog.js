a405fea55c5f37048dc0a1650d37f317484ff73e
TatanJonasEdu <comercio92jasg@gmail.com>
2025-11-27 13:16:17 +0100

    RUTA C

--- Backend/routes/rutasBlog.js
+++ Backend/routes/rutasBlog.js
@@ -1,1 +1,17 @@
-const ex
+const express = require('express');
+const router = express.Router();
+const blogController = require('../controllers/blog.controller');
+
+// Yo creo un nuevo post
+router.post('/', blogController.crearPost);
+
+// Yo obtengo posts aleatorios
+router.get('/aleatorios', blogController.obtenerPostsAleatorios);
+
+// Yo obtengo un post por su ID
+router.get('/:id', blogController.obtenerPostPorId);
+
+// Yo obtengo todos los posts (con paginación)
+router.get('/', blogController.obtenerPosts);
+
+module.exports = router;
